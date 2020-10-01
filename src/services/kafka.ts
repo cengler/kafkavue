e0931b2adb5709d6b00580409954e11a64d00315
christian.engler <christian.engler@despegar.com>
2020-10-01 20:56:24 -0300

    snappy codec + read Messages

--- src/services/kafka.ts
+++ src/services/kafka.ts
@@ -1,4 +1,8 @@
-import { Kafka } from 'kafkajs'
+import { Consumer, Kafka, CompressionTypes, CompressionCodecs } from 'kafkajs'
+// @ts-ignore
+import SnappyCodec from 'kafkajs-snappy'
+
+CompressionCodecs[CompressionTypes.Snappy] = SnappyCodec
 
 const getTopics = (hosts: string) => {
   const kafka = new Kafka({
@@ -9,6 +13,27 @@ const getTopics = (hosts: string) => {
   // .then(ts => kafka.admin().fetchTopicMetadata({ topics: ts }))
 }
 
+const getMessages = async (brokers: string[], groupId: string, topic: string, cb: Function) => {
+  const kafka = new Kafka({
+    clientId: 'kafkavue',
+    brokers
+  })
+  const consumer: Consumer = kafka.consumer({
+    groupId
+  })
+  await consumer.connect()
+  await consumer.subscribe({ topic })
+  await consumer.run({
+    eachMessage: async ({ topic, partition, message }) => {
+      cb(topic, partition, message)
+    }
+  })
+  setTimeout(() => {
+    consumer.stop()
+  }, 10000)
+}
+
 export default {
-  getTopics
+  getTopics,
+  getMessages
 }
