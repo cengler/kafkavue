a440e8f78ed692802f874e7fc9c28e25c00580a6
christian.engler <christian.engler@despegar.com>
2020-10-01 00:42:55 -0300

    MDI icon by default

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,7 @@ import store from './store'
 import vuetify from './plugins/vuetify'
 import 'roboto-fontface/css/roboto/roboto-fontface.css'
 import '@fortawesome/fontawesome-free/css/all.css'
+import '@mdi/font/css/materialdesignicons.css'
 
 Vue.config.productionTip = false
 
