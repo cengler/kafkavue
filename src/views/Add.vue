b51f89fc42249ed84879d56c7ef4aa7989815c86
christian.engler <christian.engler@despegar.com>
2020-10-04 02:27:18 -0300

    Add cluster name

--- src/views/Add.vue
+++ src/views/Add.vue
@@ -3,33 +3,42 @@
     <v-row>
       <v-col cols="12">
         <v-card>
-          <v-card-title>New Connection</v-card-title>
+          <v-form
+            ref="form"
+            v-model="valid"
+          >
+            <v-card-title>New Connection</v-card-title>
 
-          <v-card-text>
-            <v-text-field clearable label="Bootstrap Servers"
-                          v-model="bootstrapServersString"
-                          placeholder="add bootstrap servers list"
-                          :rules="[validHost]"
-            ></v-text-field>
-          </v-card-text>
-          <v-card-actions>
-            <v-btn color="info"
-                   :disabled="!validHost(bootstrapServersString)"
-                   text
-                   @click="testConnection">
-              Test Connection
-            </v-btn>
-            <v-btn color="success" text>
-              Add
-            </v-btn>
-          </v-card-actions>
+            <v-card-text>
+              <v-text-field clearable label="Cluster Name"
+                            v-model="name"
+                            placeholder="add cluster name"
+                            :rules="[notEmpty]"
+              ></v-text-field>
+              <v-text-field clearable label="Bootstrap Servers"
+                            v-model="bootstrapServersString"
+                            placeholder="add bootstrap servers list"
+                            :rules="[validHost]"
+              ></v-text-field>
+            </v-card-text>
+            <v-card-actions>
+              <v-btn color="info"
+                     :disabled="!bootstrapServersString"
+                     text
+                     @click="testConnection">
+                Test Connection
+              </v-btn>
+              <v-btn color="success" text @click="validate">
+                Add
+              </v-btn>
+            </v-card-actions>
 
-          <v-card-text>
-            <v-alert v-if="result" :type="status">
-              {{result}}
-            </v-alert>
-          </v-card-text>
-
+            <v-card-text>
+              <v-alert v-if="result" :type="status">
+                {{result}}
+              </v-alert>
+            </v-card-text>
+          </v-form>
         </v-card>
       </v-col>
     </v-row>
@@ -41,19 +50,31 @@ import { Vue, Component, Watch } from 'vue-property-decorator'
 import kafka from '@/services/kafka'
 @Component
 export default class Add extends Vue {
+  valid = false
+  name = ''
   bootstrapServersString = 'localhost:9092'
   result = ''
   status = 'success'
   validHost (v: string) {
-    return v.match(/^.+:[0-9]+/) ? true : 'bootstrap servers list must be valid'
+    return v && v.match(/^.+:[0-9]+/) ? true : 'bootstrap servers list must be valid'
+  }
+
+  notEmpty (v: string) {
+    return v ? true : 'name must have a value'
+  }
+
+  validate () {
+    if ((this.$refs.form as Vue & { validate: () => boolean }).validate()) {
+
+    }
   }
 
   testConnection () {
     this.result = ''
     kafka.test(this.bootstrapServersString)
-      .then(c => {
+      .then(() => {
         this.status = 'success'
-        this.result = `Connection Success ${c}`
+        this.result = 'Connection Success'
       })
       .catch(e => {
         this.status = 'error'
