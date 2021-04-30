a55bca3240a97b1b97d72cdbaead0fc491e1e81c
Christian Engler <christian.engler@despegar.com>
2021-04-30 19:43:01 -0300

    update libs

--- src/main.ts
+++ src/main.ts
@@ -11,7 +11,7 @@ Vue.config.productionTip = false
 
 new Vue({
   router,
-  store: store.original,
+  store: store,
   vuetify,
   render: h => h(App)
 }).$mount('#app')
--- src/store/index.ts
+++ src/store/index.ts
@@ -1,28 +1,11 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
-import module1 from './modules/module1'
-import { createDirectStore } from 'direct-vuex'
 
 Vue.use(Vuex)
 
-const { store, rootActionContext, moduleActionContext, rootGetterContext, moduleGetterContext } = createDirectStore({
-  modules: {
-    module1
-  }
-})// Export the direct-store instead of the classic Vuex store.
-
-export default store// The following exports will be used to enable types in the implementation of actions.
-
-export {
-  rootActionContext,
-  moduleActionContext,
-  rootGetterContext,
-  moduleGetterContext
-}// The following lines enable types in the injected store '$store'.
-
-export type AppStore = typeof store
-declare module 'vuex' {
-  interface Store<S> {
-    direct: AppStore;
-  }
-}
+export default new Vuex.Store({
+  state: {},
+  mutations: {},
+  actions: {},
+  modules: {}
+})
